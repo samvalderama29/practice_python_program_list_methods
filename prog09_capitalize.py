a2bf861aee4f318f289fdb6fea87b82f0adea5d9
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 22:39:29 +0800

    init: added program description, user input handling, and first-letter capitalization using slicing in prog09_capitalize.py

--- prog09_capitalize.py
+++ prog09_capitalize.py
@@ -1,1 +1,13 @@
-# Prog09: capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+# Prog09: capitalize() makes the first letter of the string, capital letter. And all other letter in small case.
+#         Create a program that do the same functionality without using capitalize() function.
+
+print("Make the first letter capital without using capitalize()")
+
+word = input("Enter a word: ")
+
+if word:
+    capital_letter = word[0].upper() + word[1:].lower()
+else:
+    capital_letter = word
+
+print(f"Capitalized word: {capital_letter}")
