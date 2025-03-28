48b966b76d26c37d8ee10cb8f81b7903c47f832e
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 21:20:16 +0800

    feat: added space padding to string based on user-defined parameter on prog06_ljust.py

--- prog06_ljust.py
+++ prog06_ljust.py
@@ -4,3 +4,12 @@ print("Add space characters without using ljust()")
 
 word = input("Enter a word: ")
 
+count_characters = int(input("Enter a number to specify the function parameter: "))
+
+space_word = count_characters * " "
+
+if len(word) >= count_characters:
+    print("Insuffiecient space. Input a bigger number for function parameter")
+else:
+    complete_word = count_characters - len(word)
+    print(f"String with space characters: {word + (" " * complete_word)}")
