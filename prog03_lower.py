fa4541566221d04f4b794da2cb6e581af1434041
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 19:44:21 +0800

    feat: create a variable to store letters in prog03_lower.py

--- prog03_lower.py
+++ prog03_lower.py
@@ -3,3 +3,6 @@
 print("Converts string into lowers case without using lower()")
 
 word = input("Enter a word (in capital or title case format): ")
+
+uppercase = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+lowercase = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
