c73c2378b2989d6807df691f594bcab262d296db
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 20:32:35 +0800

    chore: initial program implementation, debugging in progress in prog05_endswith.py

--- prog05_endswith.py
+++ prog05_endswith.py
@@ -4,3 +4,10 @@ print("Math the end part of the string without using endswith()")
 
 word = input("Enter a word: ")
 
+endword = input("Enter the last letters of the word you input: ")
+
+initial_countword = len(word)
+
+final_countword = len(endword)
+
+if initial_countword.index() == final_countword:
