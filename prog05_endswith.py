d9b07033df21f030b8a5069d2d982204c9fb3d39
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 20:07:10 +0800

    init: add program description and variable name to store user input in prog05_endswith.py

--- prog05_endswith.py
+++ prog05_endswith.py
@@ -1,1 +1,6 @@
 # Prog05: endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+print("Math the end part of the string without using endswith()")
+
+word = input("Enter a word: ")
+
