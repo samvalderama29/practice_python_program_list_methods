f8b6d0f429a5ae3c211ac38adc2de4bbc6bc2ffb
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 20:03:44 +0800

    feat: implemented for loop and if-else logic to validate if string only contains upper case letters, else an error message will appear in prog04_isupper.py

--- prog04_isupper.py
+++ prog04_isupper.py
@@ -5,3 +5,15 @@ print("Check if all characters are on upper case without using isupper")
 word = input("Enter a word: ")
 
 uppercase = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "
+
+all_upper = True
+
+for i in range(len(word)):
+    if word[i] not in uppercase:
+        all_upper = False
+        break
+
+if all_upper:
+    print("All words are in upper case format")
+else:
+    print("Input is incorrect! Characters must be all in capital letter")
