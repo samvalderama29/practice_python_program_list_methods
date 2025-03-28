e218f620c7242aa953479ff9b353580c47f1b882
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 18:17:22 +0800

    init: add program description and variable to store user input in prog01_lstrip.py

--- prog01_lstrip.py
+++ prog01_lstrip.py
@@ -1,1 +1,5 @@
 # Prog01: lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+print("Remove space characters at the beginning of the string without using lstrip()")
+
+word = int(input("Enter a word (include spaces at the beginning): "))
