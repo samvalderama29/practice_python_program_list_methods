cbe5ebbb922c723bfda3c77a36b4e3df789e6d9f
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 20:47:35 +0800

    fix: corrected if-else logic and optimized suffix checking logic using equal_word_suffix for suffix matching in prog05_endswith.py

--- prog05_endswith.py
+++ prog05_endswith.py
@@ -3,11 +3,14 @@
 print("Math the end part of the string without using endswith()")
 
 word = input("Enter a word: ")
-
 endword = input("Enter the last letters of the word you input: ")
 
 initial_countword = len(word)
-
 final_countword = len(endword)
 
-if initial_countword.index() == final_countword:
+equal_word_suffix = word[initial_countword - final_countword:]
+
+if equal_word_suffix == endword:
+    print("The words end with the given suffix")
+else:
+    print("The word does not end with the given suffix")
