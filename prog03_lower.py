723bd4d7ed1e7395e6cfdf09390893f47b60cf25
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 19:22:18 +0800

    init: add program description and variable user input in prog03_lower.py

--- prog03_lower.py
+++ prog03_lower.py
@@ -1,1 +1,5 @@
 # Prog03: lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+print("Converts string into lowers case without using lower()")
+
+word = input("Enter a word (in capital or title case format): ")
