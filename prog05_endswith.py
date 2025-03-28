cb5c405f8035801b306dd2e0d546133a28635ad2
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 20:57:35 +0800

    docs: add comments to explain prog05_endswith.py

--- prog05_endswith.py
+++ prog05_endswith.py
@@ -1,15 +1,20 @@
 # Prog05: endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
+# Print message that explains the purpose of the program
 print("Math the end part of the string without using endswith()")
 
+# Get input from the user
 word = input("Enter a word: ")
 endword = input("Enter the last letters of the word you input: ")
 
+# Get the length of the word and suffix
 initial_countword = len(word)
 final_countword = len(endword)
 
+# Extract the last part of the word manually
 equal_word_suffix = word[initial_countword - final_countword:]
 
+# Compare manually
 if equal_word_suffix == endword:
     print("The words end with the given suffix")
 else:
