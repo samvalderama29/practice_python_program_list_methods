f845bc7b8bf83a614fde5665e3cefbfe162a7b29
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 21:48:14 +0800

    init: added initial implementation of the program

--- prog07_center.py
+++ prog07_center.py
@@ -1,1 +1,11 @@
-# Prog07: center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+# Prog07: center() add space characters at the beginning and at the end of the string to print the string at the center.
+#         Create a program that do the same functionality without using center() function.
+
+print("Add space at the beginning and end of the string without using center()")
+
+word = input("Enter a word: ")
+
+space_1 = "     "
+space_2 = "     "
+
+print(space_1 + word + space_2)
