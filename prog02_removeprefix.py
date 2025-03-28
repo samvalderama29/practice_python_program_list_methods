f86615fa6fa07ef5c6782508e0d9901a0273bf46
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 18:58:31 +0800

    init: add program description and variable to store user input in prog02_removeprefix.py

--- prog02_removeprefix.py
+++ prog02_removeprefix.py
@@ -1,1 +1,7 @@
 # Prog02: removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+print("Remove characters at the beginning of the string without using removeprefix()")
+
+word = input("Enter a word: ")
+
+prefixword = input("Type the word you want to remove from the start of the string: ")
