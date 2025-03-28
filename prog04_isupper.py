1c20f9f1e0b1fa864c691634018804542e46eb87
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 19:49:53 +0800

    init: add program description and variable to store user input in prog04_isupper.py

--- prog04_isupper.py
+++ prog04_isupper.py
@@ -1,1 +1,5 @@
 # Prog04: isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+print("Check if all characters are on upper case without using isupper")
+
+word = input("Enter a word: ")
