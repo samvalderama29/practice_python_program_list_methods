fef4111b179d131d332bafa4885db65853158a03
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-28 21:04:09 +0800

    init: add program description and variable name to store user input in prog06_ljust.py

--- prog06_ljust.py
+++ prog06_ljust.py
@@ -1,1 +1,6 @@
 # Prog06: ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+print("Add space characters without using ljust()")
+
+word = input("Enter a word: ")
+
