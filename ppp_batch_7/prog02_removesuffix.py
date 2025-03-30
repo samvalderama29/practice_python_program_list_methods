b32ee0e9e097bbe8da7b6e59d65112f93b09c566
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-30 11:16:58 +0800

    docs: add comments in prog02_removesuffix.py

--- ppp_batch_7/prog02_removesuffix.py
+++ ppp_batch_7/prog02_removesuffix.py
@@ -1,17 +1,22 @@
 # Prog02: removesuffix() remove the characters at the end of the string that matches the function parameter.
 #         Create a program that do the same functionality without using removesuffix() function.
 
+# Print message that explains the purpose of the program
 print("Remove the characters at the end of the string without using removesuffix()")
 
+# Get input string from the user
 word = input("Enter a word: ")
 
+# Ask the user what suffix to remove
 suffixword = input("Enter the suffix you want to remove from the string: ")
 
+# Calculate the length of the word and the suffix
 word_count = len(word)
 suffixword_count = len(suffixword)
 
+# Check if the word ends with the given suffix
 if word[word_count - suffixword_count:] == suffixword:
-    finalword = word[:word_count - suffixword_count]
+    finalword = word[:word_count - suffixword_count] # Remove the suffix using slicing
     print(f"String after suffix removal: {finalword}")
 else:
     print("Invalid suffix. The suffix you input cannot be found in the end of the word. Try again.")
