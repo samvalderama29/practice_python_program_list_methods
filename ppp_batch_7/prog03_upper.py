1e7f8bee3689d14c8fd9162db99243bb05de0899
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-30 11:31:04 +0800

    feat: add for loop and replace method to convert lowercase to uppercase, fix grammar in description in prog03_upper.py

--- ppp_batch_7/prog03_upper.py
+++ ppp_batch_7/prog03_upper.py
@@ -1,9 +1,14 @@
 # Prog03: upper() converts all characters of the string into upper case.
 #         Create a program that do the same functionality without using upper() function.
 
-print("Convert all characters of the string into uper case without using upeer()")
+print("Convert all characters of the string into upper case without using upper()")
 
 word = input("Enter a word: ")
 
 uppercase = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 lowercase = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+
+for i in range(len(uppercase)):
+    word = word.replace(lowercase[i], uppercase[i])
+
+print(f"String in uppercase: {word}")
