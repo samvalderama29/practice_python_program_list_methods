30f71f87d895913f879c232d7cc30e4ede499c5c
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-30 10:44:38 +0800

    init: add program description and variable to store user input in prog02_removesuffix.py

--- ppp_batch_7/prog02_removesuffix.py
+++ ppp_batch_7/prog02_removesuffix.py
@@ -1,1 +1,8 @@
-# Prog02: removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+# Prog02: removesuffix() remove the characters at the end of the string that matches the function parameter.
+#         Create a program that do the same functionality without using removesuffix() function.
+
+print("Remove the characters at the end of the string without using removesuffix()")
+
+word = input("Enter a word: ")
+
+suffixword = input("Enter the suffix you want to remove from the string: ")
