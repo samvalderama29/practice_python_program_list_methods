e8e67f524be39dc2c697b1a6b93a4c7a24ccf730
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-30 11:27:35 +0800

    init: add program description, variable to store user input, and variable for alphabetical letters in prog03_upper.py

--- ppp_batch_7/prog03_upper.py
+++ ppp_batch_7/prog03_upper.py
@@ -1,1 +1,9 @@
-# Prog03: upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+# Prog03: upper() converts all characters of the string into upper case.
+#         Create a program that do the same functionality without using upper() function.
+
+print("Convert all characters of the string into uper case without using upeer()")
+
+word = input("Enter a word: ")
+
+uppercase = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+lowercase = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
