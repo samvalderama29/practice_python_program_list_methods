749ba9c4987b107167d0332d966734cead2f9e0e
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-30 10:24:54 +0800

    init: add program description and variable to store user input and count of spaces in prog01_rstrip.py

--- ppp_batch_7/prog01_rstrip.py
+++ ppp_batch_7/prog01_rstrip.py
@@ -1,1 +1,7 @@
 # Prog01: rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+print("Remove space at the end of the string without using rstrip()")
+
+word = input("Enter a word (include spaces at the end): ")
+
+space = 0
