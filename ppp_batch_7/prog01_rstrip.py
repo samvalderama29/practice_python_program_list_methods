ec45120b69467a4d44a63ed3fe06e19596ce991d
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-30 10:34:41 +0800

    feat: add initial code, debugging in progress due to errors in prog01_rstrip.py

--- ppp_batch_7/prog01_rstrip.py
+++ ppp_batch_7/prog01_rstrip.py
@@ -5,3 +5,10 @@ print("Remove space at the end of the string without using rstrip()")
 word = input("Enter a word (include spaces at the end): ")
 
 space = 0
+
+while space < len(word) and word[space] in (" ", "\t"):
+    space =+ 1
+
+removespace = word[:space]
+
+print(f"String without ending spaces: {removespace})
