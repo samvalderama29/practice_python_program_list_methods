e1f08fdcc9a8ff37ab64a2d80e569565904da8a5
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-30 10:50:41 +0800

    feat: add initial code, debugging in progress as stripped word is not in the printed output in prog02_removesuffix.py

--- ppp_batch_7/prog02_removesuffix.py
+++ ppp_batch_7/prog02_removesuffix.py
@@ -6,3 +6,9 @@ print("Remove the characters at the end of the string without using removesuffix
 word = input("Enter a word: ")
 
 suffixword = input("Enter the suffix you want to remove from the string: ")
+
+if word in suffixword:
+    finalword = word.rstrip(suffixword)
+    print(f"String after suffix removal: {finalword}")
+else:
+    print("Invalid suffix. The suffix you input cannot be found in the word. Try again.")
