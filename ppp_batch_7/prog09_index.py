a2815c7576ae8ff330d8f8b446a59c08cb15fca1
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 10:02:13 +0800

    init: add program description and variable for user input and to initialize the string in prog09_index.py

--- ppp_batch_7/prog09_index.py
+++ ppp_batch_7/prog09_index.py
@@ -1,1 +1,8 @@
-# Prog09: index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+# Prog09: index() return the first location of the function parameter in the string.
+#         Create a program that do the same functionality without using index() function.
+
+print("Return the first location of the string without using index()")
+
+word = input("Enter a word: ")
+
+word_position = -1
