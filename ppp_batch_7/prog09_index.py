2bd28a74dbab436d70d51c9647bd9088282b81d1
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 10:10:54 +0800

    feat: add if-else logic for output handling, fix grammar and variable names in prog09_index.py

--- ppp_batch_7/prog09_index.py
+++ ppp_batch_7/prog09_index.py
@@ -5,11 +5,16 @@ print("Return the first location of the string without using index()")
 
 word = input("Enter a word: ")
 
-find_letter = input("Enter a letter you want to find")
+find_letter = input("Enter a letter you want to find: ")
 
 word_position = -1
 
 for i in range(len(word)):
     if word[i] == find_letter:
-        position = i
+        word_position = i
         break
+
+if word_position != -1:
+    print(f"The first occurrence of '{find_letter}' is at index {word_position}")
+else:
+    print(f"The letter '{find_letter}' was not found in '{word}'")
