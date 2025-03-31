2461d2b92e629f4f84844da013c7bf4b6f4cefb3
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 08:25:21 +0800

    init: add program description, variable to store input, and variable for alphabetical letters in prog04_islower.py

--- ppp_batch_7/prog04_islower.py
+++ ppp_batch_7/prog04_islower.py
@@ -1,1 +1,7 @@
 # Prog04: islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+print("Check if all characters are on lower case without using islower()")
+
+word = input("Enter a word: ")
+
+lowercase = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
