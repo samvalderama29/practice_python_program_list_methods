044e663793dfed32a5a25eb1ff34d13f128138c7
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 10:25:51 +0800

    feat: implemented for loop and if-else logic for reverse search, added word position variable, and fixed minor issues in prog10_rindex.py

--- ppp_batch_7/prog10_rindex.py
+++ ppp_batch_7/prog10_rindex.py
@@ -5,4 +5,17 @@ print("Find the last occurrence in a string without using rindex()")
 
 word = input("Enter a word: ")
 
-position = -1
+find_letter = input("Enter a letter you want to find: ")
+
+word_position = -1
+
+for i in range(len(word) -1, -1, -1):
+    if word[i] == find_letter:
+        word_position = i
+        break
+
+# Display the result
+if word_position != -1:
+    print(f"The last occurrence of '{find_letter}' is at index {word_position}")
+else:
+    print(f"The letter '{find_letter}' was not found in '{word}'")
