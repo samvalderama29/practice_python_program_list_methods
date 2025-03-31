adccc590e2931619f917b91659ad52ab40ada10f
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 09:27:13 +0800

    init: add program description, variable for user input in prog08_count.py

--- ppp_batch_7/prog08_count.py
+++ ppp_batch_7/prog08_count.py
@@ -1,1 +1,8 @@
 # Prog08: count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+print("Identify how many times a string appear without using count()")
+
+word = input("Enter a word: ")
+
+find_letter = input("Enter a letter you want to find in the word: ")
+
