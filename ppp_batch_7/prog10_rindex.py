24585f1be3ceff9dd5412757e16267ee2245c2f1
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 10:22:40 +0800

    init: add program description and variable to store user input in prog10_rindex.py

--- ppp_batch_7/prog10_rindex.py
+++ ppp_batch_7/prog10_rindex.py
@@ -1,1 +1,8 @@
-# Prog10: rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+# Prog10: rindex() return the first location of the function parameter in the string starting from the last character.
+#         Create a program that do the same functionality without using rindex() function.
+
+print("Find the last occurrence in a string without using rindex()")
+
+word = input("Enter a word: ")
+
+position = -1
