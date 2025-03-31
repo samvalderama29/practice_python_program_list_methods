af61ddba8dde184f563a17f366fa5b1e0d3c405a
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 10:28:31 +0800

    docs: add comments in prog10_rindex.py

--- ppp_batch_7/prog10_rindex.py
+++ ppp_batch_7/prog10_rindex.py
@@ -1,18 +1,23 @@
 # Prog10: rindex() return the first location of the function parameter in the string starting from the last character.
 #         Create a program that do the same functionality without using rindex() function.
 
+# Print message that explains the purpose of the program
 print("Find the last occurrence in a string without using rindex()")
 
+# Get user input
 word = input("Enter a word: ")
 
+# Ask user to input a letter to search for within the word
 find_letter = input("Enter a letter you want to find: ")
 
-word_position = -1
+# Initialize a variable to track position
+word_position = -1 # Default to -1 if the character is not found
 
-for i in range(len(word) -1, -1, -1):
+# Iterate through the string in reverse order to find the last occurrence
+for i in range(len(word) -1, -1, -1): # Start from the last index and move backward
     if word[i] == find_letter:
-        word_position = i
-        break
+        word_position = i  # Store the first matching index from the end
+        break # Stop the loop after finding the last occurrence
 
 # Display the result
 if word_position != -1:
