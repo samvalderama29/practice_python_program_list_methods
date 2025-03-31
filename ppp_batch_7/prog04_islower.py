31580c35393ef5934f6966134b99860e352be0bc
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 08:30:11 +0800

    docs: add comments to explain prog04_islower.py

--- ppp_batch_7/prog04_islower.py
+++ ppp_batch_7/prog04_islower.py
@@ -1,18 +1,24 @@
 # Prog04: islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 
+# Print message that explains the purpose of the program
 print("Check if all characters are on lower case without using islower()")
 
+# Get input from the user
 word = input("Enter a word: ")
 
+# Define lowercase letters
 lowercase = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 
+# Variable to track if all characters are lowercase
 all_lower = True
 
-for i in range(len(word)):
-    if word[i] not in lowercase:
+# Loop through the string using index positions
+for i in range(len(word)): # Using len() to determine string length
+    if word[i] not in lowercase: # If a character is not lowercase and not a space
         all_lower = False
-        break
+        break # Exit loop early for efficiency
 
+# Print the result
 if all_lower:
     print("True")
 else:
