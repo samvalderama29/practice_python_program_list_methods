ab1b9b207601c99f439b896ffacab29f63aa255e
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 08:41:40 +0800

    init: add program description and variable for user input in prog06_rjust.py

--- ppp_batch_7/prog06_rjust.py
+++ ppp_batch_7/prog06_rjust.py
@@ -1,1 +1,8 @@
-# Prog06: rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+# Prog06: rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter.
+#         Create a program that do the same functionality without using rjust() function.
+
+print("Add space characters without using rjust()")
+
+word = input("Enter a word: ")
+
+count_characters = int(input("Enter a number to specify the function parameter: "))
