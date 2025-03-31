e939210b0a53f7f72b0555674e3cd56b92f58ee7
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 08:34:10 +0800

    init: add program description, user input variable, and word length counter in prog05_startswith.py

--- ppp_batch_7/prog05_startswith.py
+++ ppp_batch_7/prog05_startswith.py
@@ -1,1 +1,10 @@
-# Prog05: startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+# Prog05: startswith() check if the string beginning part matches the function parameter.
+#         Create a program that do the same functionality without using startswith() function.
+
+print("Find the beginning part of the string without using startswith()")
+
+word = input("Enter a word: ")
+endword = input("Enter the last letters of the word you input: ")
+
+initial_countword = len(word)
+final_countword = len(endword)
