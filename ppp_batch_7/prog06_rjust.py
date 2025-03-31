3a103a73801a23b23c66795122a751dcbd2aec6a
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-31 08:44:31 +0800

    feat: add if-else logic for space padding with validation for insufficient space in prog06_rjust.py

--- ppp_batch_7/prog06_rjust.py
+++ ppp_batch_7/prog06_rjust.py
@@ -6,3 +6,9 @@ print("Add space characters without using rjust()")
 word = input("Enter a word: ")
 
 count_characters = int(input("Enter a number to specify the function parameter: "))
+
+if len(word) >= count_characters:
+    print("Insuffiecient space. Input a bigger number for function parameter")
+else:
+    complete_word = count_characters - len(word)
+    print(f"String with space characters: {(" " * complete_word) + word}")
